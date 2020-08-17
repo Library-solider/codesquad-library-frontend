ccd3cae6f590bfb0444fc1c3c67ec2d6d67f9f1d
choijangho <jhchoi1115@gmail.com>
2020-08-18 06:21:47 +0900

    [#54] style : SearchForm File searchStyle 로 이동

---
+++ src/components/search/searchStyle.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+export const SearchForm = styled.div`
+  display: flex;
+  width: 50vw;
+  position: relative;
+
+  input {
+    width: inherit;
+    padding: ${({ theme: { paddings } }) => paddings.lg};
+    border-top-left-radius: ${({ theme: { radius } }) => radius.xSmall};
+    border-bottom-left-radius: ${({ theme: { radius } }) => radius.xSmall};
+  }
+
+  .search_btn {
+    padding-right: ${({ theme: { paddings } }) => paddings.small};
+    background-color: ${({ theme: { colors } }) => colors.white};
+    border-top-right-radius: ${({ theme: { radius } }) => radius.xSmall};
+    border-bottom-right-radius: ${({ theme: { radius } }) => radius.xSmall};
+
+    svg {
+      font-size: ${({ theme: { fontSizes } }) => fontSizes.xxxl};
+    }
+    :hover {
+      color: ${({ theme: { colors } }) => colors.blue};
+    }
+  }
+
+  @media ${({ theme: { device } }) => device.mobileL} {
+    margin-top: ${({ theme: { margins } }) => margins.base};
+    width: 100%;
+    order: 2;
+  }
+`;
+
+export const SearchHistoryWrapper = styled.ul`
+  position: absolute;
+  z-index: 2;
+  top: 110%;
+  width: 100%;
+  border-radius: ${({ theme: { radius } }) => radius.xSmall};
+  background-color: ${({ theme: { colors } }) => colors.white};
+  box-shadow: ${({ theme: { boxShadow } }) => boxShadow.boxShadow_1};
+
+  li {
+    padding: ${({ theme: { paddings } }) => paddings.lg};
+  }
+
+  .recent_search {
+    color: ${({ theme: { colors } }) => colors.gray_2};
+  }
+
+  .none_history {
+    display: flex;
+    justify-content: center;
+    color: ${({ theme: { colors } }) => colors.gray_2};
+    padding: ${({ theme: { paddings } }) => paddings.xxxl};
+  }
+
+  .clear_history {
+    button {
+      color: ${({ theme: { colors } }) => colors.blue_2};
+      :hover {
+        opacity: 0.7;
+      }
+    }
+  }
+`;
+
+export const HistoryItemWrapper = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${(props) => props.isActive && props.theme.colors.gray_3};
+
+  svg {
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.xl};
+    color: ${({ theme: { colors } }) => colors.gray_2};
+  }
+`;
