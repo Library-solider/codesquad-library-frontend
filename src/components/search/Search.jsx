c3a8cf72bebe6fb0d0b494c5860338c0815436c8
choijangho <jhchoi1115@gmail.com>
2020-08-18 06:27:20 +0900

    [#54] feat : Search Component 주요 기능 추가
    - Outside Click Pattern : 검색창 외부 Click시 Toggle이 Close되는 기능
    - LocalStorage Event : LocalStorage에 값을 저장 , 삭제 , 전체 삭제 기능 추가
    - Input Event : Input의 keyDown , onChange  , onFocus 기능 추가

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,67 +1,125 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import querystring from "query-string";
 
-import { SearchForm } from "../../styles/SearchForm";
+import SearchHistory from "./SearchHistory";
+
+import { SearchForm } from "./searchStyle";
 import { FiSearch } from "react-icons/fi";
+
+import { LOCALSTORAGE_KEYS } from "../../constants/localStorage";
+
+const HISTORY_FIRST_INDEX = 0;
+const HISTORY_LAST_INDEX = 4;
 
 const Search = () => {
   const history = useHistory();
-
+  const toggleContainer = useRef();
   const [isToggle, setIsToggle] = useState(false);
   const [keyword, setKeyword] = useState("");
+  const [historyPosition, setHistoryPosition] = useState(null);
+  const [searchHistory, updateSearchHistory] = useState(() => {
+    const history = localStorage.getItem(LOCALSTORAGE_KEYS.SEARCH_HISTORY);
 
-  const parsedSearchQueries = querystring.parse(history.location.search);
-  const [searchHistory, setSearchHistory] = useState(
-    JSON.parse(localStorage.getItem("searchHistory"))
-  );
-
-  const updateSearchHistory = (historyRecord, keyword) => {
-    if (historyRecord) {
-      const newHistory = searchHistory;
-      newHistory.unshift(keyword);
-      setSearchHistory(newHistory);
+    if (history) {
+      return JSON.parse(history);
     } else {
-      setSearchHistory([keyword]);
+      return [];
     }
+  });
+  const parsedSearchQueries = querystring.parse(history.location.search);
+
+  // localStorage Event
+  const clearSearchHistory = () => updateSearchHistory([]);
+  const addSearchHistory = (item) => {
+    const newSearchHistory = [...new Set([item, ...searchHistory])].slice(0, 5);
+    updateSearchHistory(newSearchHistory);
+  };
+  const removeSearchHistory = (index) => {
+    updateSearchHistory([...searchHistory].filter((_, idx) => idx !== index));
   };
 
+  // Input Event
   const onFocusInput = () => setIsToggle(true);
-  const onBlurInput = () => setIsToggle(false);
   const onChangeInput = (e) => setKeyword(e.target.value);
 
+  // key press Event
+  const moveSearchResult = (keyword) => {
+    history.push(`/search?q=${keyword}`);
+    setIsToggle(false);
+    setHistoryPosition(null);
+    addSearchHistory(keyword);
+  };
   const onKeyDownInput = (e) => {
     switch (e.key) {
       case "Enter":
-        history.push(`/search?q=${keyword}`);
-        setIsToggle(false);
-        e.target.blur();
-        updateSearchHistory(searchHistory, keyword);
+        if (keyword) {
+          moveSearchResult(keyword);
+          e.target.blur();
+        }
         break;
       case "ArrowUp":
+        if (historyPosition === null && searchHistory.length) {
+          setHistoryPosition(searchHistory.length - 1);
+        } else if (historyPosition === HISTORY_FIRST_INDEX) {
+          setHistoryPosition(null);
+        } else {
+          setHistoryPosition(historyPosition - 1);
+        }
         break;
       case "ArrowDown":
+        if (historyPosition === null && searchHistory.length) {
+          setHistoryPosition(HISTORY_FIRST_INDEX);
+        } else if (historyPosition === HISTORY_LAST_INDEX) {
+          setHistoryPosition(null);
+        } else {
+          setHistoryPosition(historyPosition + 1);
+        }
         break;
       default:
         return;
     }
   };
 
+  // outSide Click Pattern
+  const onBlurSearchHistory = (e) => {
+    if (isToggle && !toggleContainer.current.contains(e.target))
+      setIsToggle(false);
+  };
+
   useEffect(() => {
-    if (parsedSearchQueries.q) setKeyword(parsedSearchQueries.q);
+    if (historyPosition === null) {
+      setKeyword("");
+    } else {
+      setKeyword(searchHistory[historyPosition]);
+    }
+  }, [historyPosition]);
+
+  useEffect(() => {
+    setKeyword(parsedSearchQueries.q);
   }, [parsedSearchQueries.q]);
 
   useEffect(() => {
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    localStorage.setItem(
+      LOCALSTORAGE_KEYS.SEARCH_HISTORY,
+      JSON.stringify(searchHistory)
+    );
   }, [searchHistory]);
 
+  useEffect(() => {
+    window.addEventListener("click", onBlurSearchHistory);
+
+    return () => {
+      window.removeEventListener("click", onBlurSearchHistory);
+    };
+  });
+
   return (
-    <SearchForm>
+    <SearchForm ref={toggleContainer}>
       <input
         type="text"
         placeholder="원하는 책의 제목을 검색해 보세요 !"
         onFocus={onFocusInput}
-        onBlur={onBlurInput}
         onKeyDown={onKeyDownInput}
         onChange={onChangeInput}
         value={keyword}
@@ -69,7 +127,16 @@ const Search = () => {
       <button className="search_btn">
         <FiSearch />
       </button>
-      {isToggle && <ul className="search_result"></ul>}
+      {isToggle && (
+        <SearchHistory
+          searchHistory={searchHistory}
+          currentPosition={historyPosition}
+          moveSearchResult={moveSearchResult}
+          clearSearchHistory={clearSearchHistory}
+          removeSearchHistory={removeSearchHistory}
+          updateCurrentPosition={setHistoryPosition}
+        />
+      )}
     </SearchForm>
   );
 };
