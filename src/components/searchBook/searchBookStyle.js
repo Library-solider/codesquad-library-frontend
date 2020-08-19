5b78d94a651d948648a5517d1898ef1881ae1c49
choijangho <jhchoi1115@gmail.com>
2020-08-19 10:35:07 +0900

    [#2] feat : NoneResultWrapper Style Component 추가

--- src/components/searchBook/searchBookStyle.js
+++ src/components/searchBook/searchBookStyle.js
@@ -28,3 +28,24 @@ export const SearchBookInner = styled.div`
     }
   }
 `;
+
+export const NoneResultWrapper = styled.div`
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 92px;
+
+  svg {
+    font-size: 10rem;
+    margin: ${({ theme: { margins } }) => margins.base};
+  }
+  h1 {
+    text-align: center;
+    font-size: ${({ theme: { fontSizes } }) => fontSizes.xxxl};
+    color: ${({ theme: { colors } }) => colors.gray_2};
+    font-weight: bold;
+  }
+`;
