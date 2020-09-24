4d92c201f24a29ac4607b992886c82b7a6ab9d05
choijangho <jhchoi1115@gmail.com>
2020-09-25 02:28:26 +0900

    Docs : 목데이터 삭제 , 서버 응답 데이터로 변경

--- src/components/mypage/MyPage.jsx
+++ src/components/mypage/MyPage.jsx
@@ -10,26 +10,6 @@ import MyRental from "./MyRental";
 
 import { GET_OPTION } from "../../constants/fetch";
 
-const MOCK_DATA = {
-  status: true,
-  statusCode: 1,
-  statusMessage: "OK",
-  data: {
-    name: "Sunny92",
-    email: "thstkd92@gmail.com",
-    avatarUrl: "https://avatars1.githubusercontent.com/u/49144662?v=4",
-    role: "USER",
-    requested: false,
-    rentalBookResponse: [
-      {
-        title: "(명쾌한 설명과 풍부한 그림으로 배우는) TCP/IP 쉽게 더 쉽게",
-        beginDate: "2020-09-01",
-        returnDate: "2020-09-15",
-      },
-    ],
-  },
-};
-
 const MYPAGE_API = "https://backend.librarycodesquad.com/v1/user/mypage";
 
 const MyPage = () => {
@@ -38,15 +18,13 @@ const MyPage = () => {
   if (error) return <ErrorPage status={error.status} />;
   if (!response) return <Loading />;
 
+  const { name, email, avatarUrl, rentalBookResponse } = response;
+
   return (
     <MyPageWrapper>
       <MyPageInner>
-        <MyProfile
-          avatarUrl={MOCK_DATA.data.avatarUrl}
-          name={MOCK_DATA.data.name}
-          email={MOCK_DATA.data.email}
-        />
-        <MyRental rentalList={MOCK_DATA.data.rentalBookResponse} />
+        <MyProfile avatarUrl={avatarUrl} name={name} email={email} />
+        <MyRental rentalList={rentalBookResponse} />
       </MyPageInner>
     </MyPageWrapper>
   );
